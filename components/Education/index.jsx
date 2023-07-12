d5158f0509eea80364b69e5bde0cabe575427a39
Artur Karpenko <vaselisk999@gmail.com>
2023-07-12 23:37:01 +0100

    Update education section look and feel

--- components/Education/index.jsx
+++ components/Education/index.jsx
@@ -8,36 +8,66 @@ const Education = ({ education }) => (
     <h2 className={`${baseClassName}__heading`}>Education</h2>
 
     <ul className={`${baseClassName}__institutions`}>
-      {education.map(({ name, dates, diplomas, description, skills }, index) => (
-        <li key={index} className={`${baseClassName}__institutions__institution`}>
-          <h3 className={`${baseClassName}__institutions__institution__name`}>{name}</h3>
+      {education.map(
+        ({
+          name, dates, diplomas, description, skills,
+        }, index) => (
+          <li
+            key={index}
+            className={`${baseClassName}__institutions__institution`}
+          >
+            <h3 className={`${baseClassName}__institutions__institution__name`}>
+              {name}
+            </h3>
 
-          <h4 className="hide-a11y">Dates attended</h4>
-          <p className={`${baseClassName}__institutions__institution__dates`}>{dates}</p>
+            <h4 className="hide-a11y">Dates attended</h4>
+            <p className={`${baseClassName}__institutions__institution__dates`}>
+              {dates}
+            </p>
 
-          <h4 className="hide-a11y">Diplomas and certifications</h4>
+            <h4 className="hide-a11y">Diplomas and certifications</h4>
 
-          <p className={`${baseClassName}__institutions__institution__description`}>{description}</p>
-          <p className={`${baseClassName}__institutions__institution__description`}>{skills}</p>
-          <ul className={`${baseClassName}__institutions__institution__diplomas`}>
-            {diplomas.map((diploma, i) => (
-              <li key={i} className={`${baseClassName}__institutions__institution__diplomas__diploma`}>
-                <img src={`./assets/${diploma}`} alt="bootcamp" title="cerificate" />
-              </li>
-            ))}
-          </ul>
-        </li>
-      ))}
+            <p
+              className={`${baseClassName}__institutions__institution__description`}
+            >
+              {description}
+            </p>
+            <p
+              className={`${baseClassName}__institutions__institution__skills`}
+            >
+              {skills}
+            </p>
+            <ul
+              className={`${baseClassName}__institutions__institution__diplomas`}
+            >
+              {diplomas.map((diploma, i) => (
+                <li
+                  key={i}
+                  className={`${baseClassName}__institutions__institution__diplomas__diploma`}
+                >
+                  <img
+                    src={`./assets/${diploma}`}
+                    alt="bootcamp"
+                    title="cerificate"
+                  />
+                </li>
+              ))}
+            </ul>
+          </li>
+        ),
+      )}
     </ul>
   </section>
 );
 
 Education.propTypes = {
-  education: PropTypes.arrayOf(PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    dates: PropTypes.string.isRequired,
-    diplomas: PropTypes.array.isRequired,
-  })).isRequired,
+  education: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      dates: PropTypes.string.isRequired,
+      diplomas: PropTypes.array.isRequired,
+    }),
+  ).isRequired,
 };
 
 export default Education;
